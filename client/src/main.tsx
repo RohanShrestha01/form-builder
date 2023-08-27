ca94b6c355739346a036f111761ecbb9408a6230
Rohan Shrestha <shrestharohan039@gmail.com>
2023-08-27 16:19:18 +0545

    feat: added react-hot-toast

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { Toaster } from 'react-hot-toast';
 
 import App from './App.tsx';
 import { BrowserRouter } from 'react-router-dom';
@@ -13,6 +14,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
         <App />
+        <Toaster position="bottom-center" />
         <ReactQueryDevtools />
       </QueryClientProvider>
     </BrowserRouter>
