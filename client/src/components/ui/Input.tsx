e4657d4e9e1adde97d471e0f62975b992b97f02c
Rohan Shrestha <shrestharohan039@gmail.com>
2023-09-04 22:59:09 +0545

    feat: dropdown menu ui component

--- client/src/components/ui/Input.tsx
+++ client/src/components/ui/Input.tsx
@@ -11,7 +11,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
       <input
         type={type}
         className={cn(
-          'flex h-9 w-full rounded-md border bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground hover:border-ring focus-visible:border-primary focus-visible:outline-none disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50',
+          'flex h-9 w-full rounded-md border bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground hover:border-ring focus-visible:border-primary focus-visible:outline-none disabled:pointer-events-none disabled:opacity-50',
           className,
         )}
         ref={ref}
